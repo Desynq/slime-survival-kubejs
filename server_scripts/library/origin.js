94eff2c20d2bf83dc9f782b33d998d1fdc5e181f
Desynq <69401694+Desynq@users.noreply.github.com>
2024-02-26 12:38:41 -0500

    Added new library function getOrigin()

---
+++ server_scripts/library/origin.js
@@ -0,0 +1,21 @@
+// priority: 1000
+
+
+
+const $OriginLayer = Java.loadClass('io.github.apace100.origins.origin.OriginLayer');
+const $OriginLayers = Java.loadClass('io.github.apace100.origins.origin.OriginLayers');
+const $ModComponents = Java.loadClass('io.github.apace100.origins.registry.ModComponents');
+const $Origin = Java.loadClass('io.github.apace100.origins.origin.Origin');
+
+
+
+/**
+ * @param {Internal.Entity} entity
+ * @param {string} originLayerString
+ */
+function getOrigin(entity, originLayerString) {
+	let originLayer = $OriginLayers.getLayer(originLayerString ?? 'origins:origin');
+	let origin = $ModComponents.ORIGIN.get(entity).getOrigin(originLayer);
+
+	return origin.getIdentifier();
+};
