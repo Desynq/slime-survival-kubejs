ddcdccab2e7558dec6a6c933aaadb765f8afeff0
Desynq <69401694+Desynq@users.noreply.github.com>
2024-03-18 15:01:18 -0400

    Added dense power to sludge

---
+++ server_scripts/datapack/slimesurvival/powers/origins/sludge/Dense.js
@@ -0,0 +1,32 @@
+// priority: 101
+
+(function () {
+	const format = Color.TEXT;
+	const defaultMaxHealth = $Attributes.MAX_HEALTH.defaultValue;
+	const targetMaxHealth = 30;
+
+	const resourceLocation = 'slimesurvival:powers/origins/sludge/dense.json';
+
+	let powerJson = {
+		name: 'Dense',
+		description: 'Sludges are composed of a large thick mucus layer that gives them a higher constitution.',
+		badges: [
+			{
+				type: 'origins:tooltip',
+				sprite: 'origins:textures/gui/badge/info.png',
+				text: `${format.red}Base max health is ${targetMaxHealth}`
+			}
+		],
+
+		type: 'origins:attribute',
+		modifier: {
+			name: 'Sludge Dense Buff',
+			attribute: 'minecraft:generic.max_health',
+			value: targetMaxHealth - defaultMaxHealth,
+			operation: 'addition'
+		}
+	};
+
+	new ApoliPower(resourceLocation, powerJson, ['slimesurvival:sludge']);
+
+})();
