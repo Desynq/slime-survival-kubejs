8723563f1fd4b53f59c3d6b304560aadc21f2b56
Desynq <69401694+Desynq@users.noreply.github.com>
2024-03-25 17:24:38 -0400

    Created a prototype for conditional attributes

--- server_scripts/datapack/slimesurvival/powers/origins/draconian/Clunky.js
+++ server_scripts/datapack/slimesurvival/powers/origins/draconian/Clunky.js
@@ -1,10 +1,13 @@
 // priority: 101
 
 
-
-let Clunky = DATAGEN.powers.origins.draconian.Clunky = {};
+var Clunky = {};
+DATAGEN.powers.origins.draconian.Clunky = Clunky;
 
 Clunky.coefficient = -0.04;
+Clunky.modifierAttribute = CombatRollClasses.$EntityAttributes_CombatRoll.DISTANCE;
+Clunky.modifierUUID = UUID.fromString('ef314a4e-54e0-484e-8b53-d1e96399613a');
+Clunky.modifierName = 'Draconian clunky power'
 
 Clunky.resourceLocation = 'slimesurvival:powers/origins/draconian/clunky.json';
 Clunky.powerType = new PowerType.Simple();
@@ -12,27 +15,27 @@ Clunky.power = new Power(Clunky.resourceLocation, Clunky.powerType)
 	.name('Clunky')
 	.description('Your scales lower your flexibility and make it more difficult to roll around and dodge attacks.')
 	.defaultOrigins('slimesurvival:draconian')
-	.addBadge(new ApoliBadgeType.Tooltip('minecraft:textures/mob_effect/slowness.png', `${Clunky.coefficient * 100 + '%'} combat roll distance per armor point`));
+	.addBadge(new ApoliBadgeType.Tooltip('minecraft:textures/mob_effect/slowness.png', `${Clunky.coefficient * 100 + '%'} combat roll distance per armor point`))
+;
+
+
+
 
 
 PlayerEvents.tick(event => {
 	const { player } = event;
 	const Clunky = DATAGEN.powers.origins.draconian.Clunky;
 
-	const attribute = CombatRollClasses.$EntityAttributes_CombatRoll.DISTANCE;
-	const modifierUUID = UUID.fromString('ef314a4e-54e0-484e-8b53-d1e96399613a');
+	const util = new AttributeModifierUtil(player, Clunky.modifierAttribute, Clunky.modifierUUID, Clunky.modifierName);
 
 	const hasPower = OriginsLibrary.isPowerActive(player, 'slimesurvival:origins/draconian/clunky');
-	const hasModifier = player.attributes.hasModifier(attribute, modifierUUID);
-
-	const modifierValue = hasModifier ? player.attributes.getModifierValue(attribute, modifierUUID) : 0;
 	const armor = player.attributes.getValue($Attributes.ARMOR);
 	const value = armor * Clunky.coefficient;
 
-	if (hasModifier && (!hasPower || modifierValue !== value)) {
-		player.attributes.getInstance(attribute).removeModifier(modifierUUID);
-	}
-	if (!hasModifier && hasPower) {
-		player.attributes.getInstance(attribute).addPermanentModifier(new $AttributeModifier(modifierUUID, 'Draconian clunky power', value, 'multiply_total'));
-	}
+	util.conditional(
+		!hasPower || util.modifierValue !== value,
+		hasPower,
+		value,
+		'multiply_total'
+	);
 });
---
+++ server_scripts/library/util/AttributeModifier.js
@@ -0,0 +1,73 @@
+// priority: 2147483647
+
+
+
+/**
+ * @param {Internal.LivingEntity} entity
+ * @param {Internal.Attribute} attribute
+ * @param {Internal.UUID} modifierUUID
+ * @param {String} modifierName
+ * @constructor
+ */
+function AttributeModifierUtil(entity, attribute, modifierUUID, modifierName) {
+	this.entity = entity;
+	this.attribute = attribute;
+	this.modifierUUID = modifierUUID;
+	this.modifierName = modifierName;
+}
+
+/**
+ * @returns {Internal.AttributeInstance}
+ */
+Object.defineProperty(AttributeModifierUtil.prototype, 'attributeInstance', {
+	get: function () {
+		return this.entity.attributes.getInstance(this.attribute);
+	}
+});
+
+/**
+ * @returns {Boolean}
+ */
+Object.defineProperty(AttributeModifierUtil.prototype, 'hasModifier', {
+	get: function () {
+		return this.entity.attributes.hasModifier(this.attribute, this.modifierUUID);
+	}
+});
+
+/**
+ * @returns {Number}
+ */
+Object.defineProperty(AttributeModifierUtil.prototype, 'modifierValue', {
+	get: function () {
+		return this.hasModifier ? this.entity.attributes.getModifierValue(this.attribute, this.modifierUUID) : 0;
+	}
+});
+
+
+AttributeModifierUtil.prototype.removeModifier = function () {
+	this.attributeInstance.removeModifier(this.modifierUUID);
+}
+
+/**
+ * @param {Number} value
+ * @param {Internal.AttributeModifier$Operation_} operation
+ */
+AttributeModifierUtil.prototype.addPermanentModifier = function (value, operation) {
+	const modifier = new $AttributeModifier(this.modifierUUID, this.modifierName, value, operation);
+	this.attributeInstance.addPermanentModifier(modifier);
+}
+
+
+/**
+ * Removes and/or adds attribute modifier based on two respective booleans.
+ * Useful for conditional attribute modifiers.
+ * @param {Boolean} shouldRemove Boolean determining whether modifier should be removed.
+ * @param {Boolean} shouldAdd Boolean determing whether modifier should be added.
+ * @param {Number} value
+ * @param {Internal.AttributeModifier$Operation_} operation
+ */
+AttributeModifierUtil.prototype.conditional = function (shouldRemove, shouldAdd, value, operation) {
+	const hasModifier = this.hasModifier;
+	if (hasModifier && shouldRemove) this.removeModifier();
+	if (!hasModifier && shouldAdd) this.addPermanentModifier(value, operation);
+}
