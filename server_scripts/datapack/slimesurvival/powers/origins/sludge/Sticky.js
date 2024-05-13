ab7391f7729d37ca618f2d691062c44c1ccf477a
Desynq <69401694+Desynq@users.noreply.github.com>
2024-05-13 05:53:09 -0400

    Commented out erroring power

--- server_scripts/datapack/slimesurvival/powers/origins/sludge/Sticky.js
+++ server_scripts/datapack/slimesurvival/powers/origins/sludge/Sticky.js
@@ -1,16 +1,16 @@
 // priority: 101
 
-function SludgeStickyPower() {
-	this.resourceLocation = 'slimesurvival:powers/origins/sludge/sticky.json';
-	this.powerType = undefined;
-	this.power = new Power(this.resourceLocation, this.powerType)
-		.name('Sticky')
-		.description('You find it difficult to move while on the ground')
-		.defaultOrigins('slimesurvival:sludge')
-		.addBadge(new ApoliBadgeType.Tooltip('minecraft:textures/item/iron_boots.png',
-			'You are unable to sprint while on the ground'
-		))
-	;
-}
+// function SludgeStickyPower() {
+// 	this.resourceLocation = 'slimesurvival:powers/origins/sludge/sticky.json';
+// 	this.powerType = undefined;
+// 	this.power = new Power(this.resourceLocation, this.powerType)
+// 		.name('Sticky')
+// 		.description('You find it difficult to move while on the ground')
+// 		.defaultOrigins('slimesurvival:sludge')
+// 		.addBadge(new ApoliBadgeType.Tooltip('minecraft:textures/item/iron_boots.png',
+// 			'You are unable to sprint while on the ground'
+// 		))
+// 	;
+// }
 
-SludgeStickyPower.INSTANCE = new SludgeStickyPower();
+// SludgeStickyPower.INSTANCE = new SludgeStickyPower();
